eddf51886624c4738f0dcb6a0f679b4738eaf366
david kruggel <david.kruggel@gmail.com>
2021-11-21 16:25:30 -0600

    Add openweather api call

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/codingsince1985/geo-golang/openstreetmap"
 	"github.com/gorilla/mux"
@@ -31,10 +36,72 @@ var StatusCheck = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) 
 var HomeHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	// Get location in lat/lon
 	location, _ := openstreetmap.Geocoder().Geocode("996 Crestwood Lane, O'Fallon, MO, 63366")
-	fmt.Printf("%.6f : %.6f", location.Lat, location.Lng)
-	s := fmt.Sprintf("%.6f : %.6f", location.Lat, location.Lng)
-	w.Write([]byte(s))
+
+	lat := fmt.Sprintf("%.6f", location.Lat)
+	lon := fmt.Sprintf("%.6f", location.Lng)
 
 	// Get weather
-	
+	// Retrieve environment variable that holds the openweather API key
+	apikey := os.Getenv("WEATHER_API_KEY")
+
+	// Build the api call to openweather
+	uri := fmt.Sprintf("https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s", lat, lon, apikey)
+	resp, err := http.Get(uri)
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	dec := json.NewDecoder(strings.NewReader(string(body)))
+
+	for {
+		var c WeatherNow
+		if err := dec.Decode(&c); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%.2f\u2103\n%.2f\u2103\n%s\n%s\n", GetCelcius(c.Current.CurrentTemp),
+			GetCelcius(c.Current.FeelsLike), GetLocalTime(c.Current.Sunrise),
+			GetLocalTime(c.Current.Sunset))
+	}
+
+	w.Write([]byte(body))
 })
+
+func GetCelcius(incoming float64) float64 {
+	return incoming - 273.15
+}
+
+func GetLocalTime(inc float64) string {
+	loc, _ := time.LoadLocation("America/Chicago")
+	return time.Unix(int64(inc), 0).UTC().In(loc).Format("3:04pm")
+}
+
+type WeatherNow struct {
+	Current WeatherInfo `json:"current"`
+}
+
+type WeatherRain struct {
+	LastHour float64 `json:"1h"`
+}
+
+type WeatherSnow struct {
+	LastHour float64 `json:"1h"`
+}
+
+type WeatherInfo struct {
+	Sunrise     float64     `json:"sunrise"`
+	Sunset      float64     `json:"sunset"`
+	CurrentTemp float64     `json:"temp"`
+	FeelsLike   float64     `json:"feels_like"`
+	Rain        WeatherRain `json:"rain"`
+	Snow        WeatherSnow `json:"snow"`
+}
