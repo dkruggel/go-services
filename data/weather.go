ed7bfef6f021a6cb99acd19891ddb8659a53eb07
david kruggel <david.kruggel@gmail.com>
2022-06-05 15:55:10 -0500

    First iteration of weather complete

--- data/weather.go
+++ data/weather.go
@@ -12,7 +12,7 @@ import (
 type Weather struct {
 	Current WeatherNow
 	Hourly  []WeatherNow
-	Daily   WeatherDaily
+	Daily   []WeatherDaily
 }
 
 type WeatherNow struct {
@@ -23,11 +23,26 @@ type WeatherNow struct {
 }
 
 type WeatherDaily struct {
+	Date      int64 `json:"dt"`
+	Sunrise   int64
+	Sunset    int64
+	Temp      Temperatures
+	Condition []Condition `json:"weather"`
 }
 
 type Condition struct {
 	ID   int
 	Main string `json:"main"`
+	Icon string
+}
+
+type Temperatures struct {
+	Day     float64
+	Min     float64
+	Max     float64
+	Night   float64
+	Evening float64 `json:"eve"`
+	Morning float64 `json:"morn"`
 }
 
 func (user *User) GetWeather() (weather Weather, err error) {
@@ -47,7 +62,7 @@ func (user *User) GetWeather() (weather Weather, err error) {
 
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 
-	fmt.Printf(string(bodyBytes)[0:750])
+	fmt.Printf(string(bodyBytes))
 
 	json.Unmarshal(bodyBytes, &weather)
 
@@ -58,10 +73,35 @@ func (weather WeatherNow) TempCelcius() (temp string) {
 	return fmt.Sprintf("%.2f", weather.Temp-273.15)
 }
 
+func (temperature Temperatures) MaxTempCelcius() (temp string) {
+	return fmt.Sprintf("%.2f", temperature.Max-273.15)
+}
+
+func (temperature Temperatures) MinTempCelcius() (temp string) {
+	return fmt.Sprintf("%.2f", temperature.Min-273.15)
+}
+
 func (weather WeatherNow) RealFeelCelcius() (temp string) {
 	return fmt.Sprintf("%.2f", weather.RealFeel-273.15)
 }
 
-func (weather WeatherNow) DisplayTime() (t int64) {
-	return int64(time.Unix(weather.Time, 0).Hour())
+func (weather WeatherNow) DisplayTime() (t string) {
+	dt := int64(time.Unix(weather.Time, 0).Hour())
+	if dt > 12 {
+		return fmt.Sprintf("%d:00p", dt%12)
+	} else if dt == 0 {
+		return "12:00a"
+	} else {
+		return fmt.Sprintf("%d:00a", dt)
+	}
+}
+
+func (weather WeatherDaily) DisplayDate() (t string) {
+	_, m, d := time.Unix(weather.Date, 0).Date()
+	w := time.Unix(weather.Date, 0).Weekday()
+	return fmt.Sprintf("%v, %v %d", w, m, d)
+}
+
+func (condition Condition) DisplayIcon() (s string) {
+	return fmt.Sprintf("https://openweathermap.org/img/wn/%s@2x.png", condition.Icon)
 }
